b3a8ccdfe65fd1ee6f7b661960417d17bbe2847a
Rohit Meshram <115627734+RohitMeshram45@users.noreply.github.com>
2024-02-02 08:52:34 +0530

    Update page.js
    Routing update commit

--- app/project/page.js
+++ app/project/page.js
@@ -6,18 +6,18 @@ import { motion } from "framer-motion";
 const Projects = () => {
     return (
         <> 
-        <h2 className=" mb-4 mt-9 text-5xl tracking-tight font-extrabold text-center text-gray-500 dark:text-slate-500">Personal Projects </h2>
+        <h2 className=" mb-5 mt-9 text-5xl tracking-tight font-extrabold text-center text-gray-500 dark:text-slate-500">Personal Projects </h2>
         <p className="mb-8 lg:mb-16  semibold text-center font-semibold text-gray-700 dark:text-gray-600 sm:text-xl">My Recent work</p>
         <section className='flex justify-center gap-28 max-sm:items-center  max-sm:flex-col'>
             <div></div>
             { /*<--firts part--> */}
             <motion.div   whileHover={{ scale: [null, 1, 1.1] }}
             transition={{ duration: 0.4 }} 
-                className=" hover:bg-slate-200 shadow-2xl border border-1 border-solid border-black max-sm:mb-5 max-sm:rounded-md  block max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-blue-300 ">
+                className=" hover:bg-slate-200  mb-10 border border-1 border-solid border-black max-sm:mb-5 max-sm:rounded-md  block max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-blue-300 ">
                 <div className="relative overflow-hidden bg-cover bg-no-repeat">
                     <img
                         className="rounded-t-lg"
-                        src={"Portfolio.png"}
+                        src={"images/Portfolio.png"}
                          />
                 </div>
                 <div className="p-6 text-center font-semibold text-slate-900">
@@ -32,11 +32,11 @@ const Projects = () => {
 
             <motion.div   whileHover={{ scale: [null, 1, 1.1] }}
             transition={{ duration: 0.4 }} 
-            className="hover:bg-slate-200  shadow-2xl border border-1 border-solid border-black max-sm:mb-5 max-sm:rounded-md  block max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-blue-300 ">
+            className="hover:bg-slate-200 mb-10 border border-1 border-solid border-black max-sm:mb-5 max-sm:rounded-md  block max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-blue-300 ">
                 <div className="relative overflow-hidden bg-cover bg-no-repeat">
                     <img
                         className="rounded-t-lg"
-                        src={"Sorting.jpg"}
+                        src={"images/Sorting.jpg"}
                         alt="" />
                 </div>
                 <div className="p-7 text-center font-semibold text-slate-900">
@@ -51,11 +51,11 @@ const Projects = () => {
 
             <motion.div   whileHover={{ scale: [null, 1, 1.1] }}
             transition={{ duration: 0.3 }} 
-            className="hover:bg-slate-200  border border-1 border-solid border-black max-sm:mb-5 max-sm:rounded-md  block max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-blue-300 ">
+            className="hover:bg-slate-200  mb-10 border border-1 border-solid border-black max-sm:mb-5 max-sm:rounded-md  block max-w-[18rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-blue-300 ">
                 <div className="relative overflow-hidden bg-cover bg-no-repeat bg-slate-400 p-2">
                     <img
                         className="rounded-t-lg"
-                        src={'ecommerce.png'}
+                        src={'images/ecommerce.png'}
                         alt="" />
                 </div>
                 <div className="p-6 text-center font-semibold text-slate-900">
